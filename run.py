3dac1cf7054bdb5296cd5384eba3851f060da267
Evan Kreutzwiser <53940800+Evan-Kreutzwiser@users.noreply.github.com>
2023-10-24 13:14:08 -0400

    Create board propositions

--- run.py
+++ run.py
@@ -36,16 +36,17 @@ class FancyPropositions:
         return f"A.{self.data}"
 
 # Call your variables whatever you want
-a = BasicPropositions("a")
-b = BasicPropositions("b")   
-c = BasicPropositions("c")
-d = BasicPropositions("d")
-e = BasicPropositions("e")
+#a = BasicPropositions("a")
+#b = BasicPropositions("b")   
+#c = BasicPropositions("c")
+#d = BasicPropositions("d")
+#e = BasicPropositions("e")
 # At least one of these will be true
 x = FancyPropositions("x")
 y = FancyPropositions("y")
 z = FancyPropositions("z")
 
+board = []
 
 # Build an example full theory for your setting and return it.
 #
@@ -54,14 +55,31 @@ z = FancyPropositions("z")
 #  what the expectations are.
 def example_theory():
     # Add custom constraints by creating formulas with the variables you created. 
-    E.add_constraint((a | b) & ~x)
+    #E.add_constraint((a | b) & ~x)
     # Implication
     E.add_constraint(y >> z)
     # Negate a formula
     E.add_constraint(~(x & y))
     # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
     # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    constraint.add_exactly_one(E, a, b, c)
+    #constraint.add_exactly_one(E, a, b, c)
+
+    # 2d array of dictionaries
+    # row#, col#, color key
+    colors = ["r", "g", "b", "y", "w", "b"]
+
+    # Build propositions for every color in every peg of the board
+    for row in range(0, 8):
+        board.append([])
+        for col in range(0, 4):
+            board[row].append({})
+            for color in colors:
+                board[row][col][color] = BasicPropositions(f"X{row}{col}" + color)
+            # Exactly one color can be present in a peg
+            constraint.add_exactly_one(E, *board[row][col].values())
+
+    # Define the contents of the first row
+    E.add_constraint(board[0][0]["r"] & board[0][1]["w"] & board[0][2]["y"] & board[0][3]["g"])
 
     return E
 
@@ -78,8 +96,10 @@ if __name__ == "__main__":
     print("   Solution: %s" % T.solve())
 
     print("\nVariable likelihoods:")
-    for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
-        # Ensure that you only send these functions NNF formulas
-        # Literals are compiled to NNF here
-        print(" %s: %.2f" % (vn, likelihood(T, v)))
+    for row in range(len(board)):
+        for col in range(len(board[row])):
+            for p in board[row][col].values():
+                # Ensure that you only send these functions NNF formulas
+                # Literals are compiled to NNF here
+                print(" %s: %.2f" % (p, likelihood(T, p)))
     print()
